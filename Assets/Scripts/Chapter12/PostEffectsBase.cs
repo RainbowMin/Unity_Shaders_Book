ae3058ce6bdd2ab87ac3d0ea580d7bc46667b940
闵杰 <123438952@qq.com>
2021-06-15 00:37:25 +0800

    ch12-1 屏幕后处理

--- Assets/Scripts/Chapter12/PostEffectsBase.cs
+++ Assets/Scripts/Chapter12/PostEffectsBase.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//所有屏幕后处理效果都需要绑定在某个摄像机上，并且我们希望在编辑器状态下也可以执行该脚本来查看效果
 [ExecuteInEditMode]
 [RequireComponent (typeof(Camera))]
 public class PostEffectsBase : MonoBehaviour {
 
-	// Called when start
+	// 为了提前检查各种资源和条件是否满足，我们在Start函数中调用CheckResources函数
+	//一些屏幕特效可能需要更多的设置，例如设置一些默认值等，可以重载Start、CheckResources或CheckSupport函数。
 	protected void CheckResources() {
 		bool isSupported = CheckSupport();
 		
@@ -33,7 +35,8 @@ public class PostEffectsBase : MonoBehaviour {
 		CheckResources();
 	}
 
-	// Called when need to create the material used by this effect
+	//由于每个屏幕后处理效果通常都需要指定一个Shader来创建一个用于处理渲染纹理的材质，因此基类中也提供了这样的方法
+	//第一个参数指定了该特效需要使用的Shader，第二个参数则是用于后期处理的材质。该函数首先检查Shader的可用性，检查通过后就返回一个使用了该Shader的材质，否则返回null
 	protected Material CheckShaderAndCreateMaterial(Shader shader, Material material) {
 		if (shader == null) {
 			return null;
---
+++ Assets/_MJ/Ch12 屏幕后处理/1 调整亮度、饱和度和对比度/MJ_BrightnessSaturationAndContrast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//调整屏幕的亮度、饱和度和对比度
+public class MJ_BrightnessSaturationAndContrast : PostEffectsBase
+{
+    public Shader briSatConShader;
+    private Material briSatConMaterial;
+    public Material material 
+    {  
+        get 
+        {
+            briSatConMaterial = CheckShaderAndCreateMaterial(briSatConShader, briSatConMaterial);
+            return briSatConMaterial;
+        }  
+    }
+
+    [Range(0.0f, 3.0f)]
+    public float brightness = 1.0f;
+
+    [Range(0.0f, 3.0f)]
+    public float saturation = 1.0f;
+
+    [Range(0.0f, 3.0f)]
+    public float contrast = 1.0f;
+}
